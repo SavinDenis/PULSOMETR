7b7c529b281dc60a384f9585f7108c4b32f4d0ce
Savin Denis <den32179@mail.kz>
2022-12-06 19:30:07 +0600

    add card

---
+++ src/js/slider.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function(){
+    $('.slider__inner').slick({
+        infinite: false,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/chevron_left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/chevron_right.svg"></button>'
+    
+      });
+  });
+      
+
+
+
+
