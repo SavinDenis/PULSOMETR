224120e880cd201b603b40541d0fb98f8125b3f5
Savin Denis <den32179@mail.kz>
2022-12-06 23:48:49 +0600

    add tabs

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,13 +1,20 @@
 
-$(document).ready(function(){
+$(document).ready(function () {
     $('.slider__inner').slick({
         infinite: false,
         prevArrow: '<button type="button" class="slick-prev"><img src="../icons/chevron_left.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="../icons/chevron_right.svg"></button>'
-    
-      });
-  });
-      
+
+    });
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
+       
+        $(this)
+            .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+            .closest('div.catalog__wrapper').find('div.catalog__cards').removeClass('catalog__cards_active').eq($(this).index()).addClass('catalog__cards_active');
+    });
+
+});
 
 
 
