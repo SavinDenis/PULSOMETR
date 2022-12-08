8d8c6d0f60dc674d47656fd062cf9c37cb4b0d63
Savin Denis <den32179@mail.kz>
2022-12-08 15:22:49 +0600

    add tabs

--- src/js/slider.js
+++ src/js/slider.js
@@ -7,12 +7,47 @@ $(document).ready(function () {
 
     });
 
+    /* Смена табов в каталоге */
+
     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
-       
+
         $(this)
             .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
             .closest('div.catalog__wrapper').find('div.catalog__cards').removeClass('catalog__cards_active').eq($(this).index()).addClass('catalog__cards_active');
     });
+
+/* Не оптимизированный код перезода подробнее-назад */
+
+    /* $('.card__link').each(function (i) {
+        $(this).on('click', function (e) {
+            e.preventDefault();
+            $('.card__main').eq(i).toggleClass('card__main_active');
+            $('.card__list').eq(i).toggleClass('card__list_active');
+        })
+    });
+
+    $('.card__list_back').each(function (i) {
+        $(this).on('click', function (e) {
+            e.preventDefault();
+            $('.card__main').eq(i).toggleClass('card__main_active');
+            $('.card__list').eq(i).toggleClass('card__list_active');
+        })
+    }); */
+
+/* Оптимизированный код перезода подробнее-назад */
+
+    function toggleSlide(card) {
+        $(card).each(function (i) {
+            $(this).on('click', function (e) {
+                e.preventDefault();
+                $('.card__main').eq(i).toggleClass('card__main_active');
+                $('.card__list').eq(i).toggleClass('card__list_active');
+            })
+        });
+    };
+
+    toggleSlide('.card__link');
+    toggleSlide('.card__list_back');
 
 });
 
